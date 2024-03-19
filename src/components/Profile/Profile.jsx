74e1e2b7af4a94b9561e7d6e8ead2b27c87fbbf9
Afanasi <narik.fara@mail.ru>
2024-03-19 09:49:57 +0200

    happy

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,23 +1,55 @@
+// import ProfileInfo from "./ProfileInfo/ProfileInfo";
+// import MyPosts from "./MyPosts/MyPosts";
+// import { useParams } from "react-router-dom";
+// import { getProfile, getStatus } from "../../redux/profile-reducer";
+// import { useDispatch, useSelector } from "react-redux";
+// import { withAuthRedirect } from "../../hoc/withAuthContainer";
+// import React, { useEffect } from "react";
+
+// const Profile = () => {
+//   let { userId } = useParams();
+//   const authorizedUserId = useSelector((state) => state.auth.userId);
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     if (!userId) {
+//       userId = authorizedUserId;
+//     }
+//     dispatch(getProfile(userId));
+//     dispatch(getStatus(userId));
+//   }, [userId, dispatch]);
+//   return (
+//     <div>
+//       <ProfileInfo />
+//       <MyPosts />
+//     </div>
+//   );
+// };
+
+// export default withAuthRedirect(Profile);
+
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPosts from "./MyPosts/MyPosts";
 import { useParams } from "react-router-dom";
 import { getProfile, getStatus } from "../../redux/profile-reducer";
 import { useDispatch, useSelector } from "react-redux";
 import { withAuthRedirect } from "../../hoc/withAuthContainer";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 const Profile = () => {
-  let { userId } = useParams();
+  const { userId } = useParams();
   const authorizedUserId = useSelector((state) => state.auth.userId);
   const dispatch = useDispatch();
+  const userIdRef = useRef(null);
 
   useEffect(() => {
-    if (!userId) {
-      userId = authorizedUserId;
+    if (!userIdRef.current) {
+      userIdRef.current = userId || authorizedUserId;
     }
-    dispatch(getProfile(userId));
-    dispatch(getStatus(userId));
-  }, [ dispatch]);
+    dispatch(getProfile(userIdRef.current));
+    dispatch(getStatus(userIdRef.current));
+  }, [userId, authorizedUserId, dispatch]);
+  
   return (
     <div>
       <ProfileInfo />
